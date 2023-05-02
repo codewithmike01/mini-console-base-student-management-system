f527f819357a6011b54659074e1c3e344b4adf6c
ginohmk <michotall95@gmail.com>
2023-05-02 13:10:29 +0100

    feat: Add update of student data

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -2,47 +2,130 @@ import csv
 from lib.student import Student
 
 def display_options():
-    print(f"""
+    print("""
     1. Display all students
     2. Update Student detail
     3. insert new Student detail
     4. Delete a Student detail
+    5. Exist
     """)
+
+def accept_update_input():
+    name = str(input('Enter Student name: '))
+    age = int(input('Enter Student age: '))
+    mentor = str(input('Enter Student mentor: '))
+    major = str(input('Enter Student major: '))
+
+    return  name, age, mentor, major
+
+
+def accept_initial_input():
+    id = str(input('Enter Student Id () : '))
+    name = str(input('Enter Student name: '))
+    age = int(input('Enter Student age: '))
+    mentor = str(input('Enter Student mentor: '))
+    major = str(input('Enter Student major: '))
+
+    return id, name, age, mentor, major
+
+
+
+
+
+def select_id():
+    id = str(input('Enter Student Id to update: '))
+    return id
+
+def valid_id_avialability(id):
+
+    for student in Student.all_student:
+        if id == student['id']:
+            return True
+    return False
 
 
 def display_student():
+    """Display all students in the csv file.
     """
-    Display all students in the csv file
-    """
+
     student_obj = {}
     students_list = []
     with open("students.csv", "r") as students:
-        
+
         for student in students:
             student_list = student.strip().split(',')
 
-            student_obj['id'] = student_list[0]  
-            student_obj['name'] =   student_list[1]    
-            student_obj['age'] =   student_list[2]    
-            student_obj['mentor'] =   student_list[3]    
-            student_obj['major'] =   student_list[4]  
-            
+            student_obj['id'] = student_list[0].strip()
+            student_obj['name'] =   student_list[1].strip()
+            student_obj['age'] =   student_list[2].strip()
+            student_obj['mentor'] =   student_list[3].strip()
+            student_obj['major'] =   student_list[4].strip()
+
             students_list.append(student_obj)
             student_obj = {}
-            
+
         Student.instantiate_students(students_list)
+        input('Hit enter to see options: ')
 
 def update_student():
-    
-    """
-    Accept Student Id 
 
-    Use stude id to map student and 
+    """Accept Student Id.
+
+    Use stude id to map student and
     update student detail
 
     Attribute: Id is str
     """
-    pass
+
+
+    id = select_id()
+
+    if valid_id_avialability(id):
+        name, age, mentor, major = accept_update_input()
+        def get_update(student):
+
+            if student['id'] == id:
+                student['name'] = name
+                student['age'] = age
+                student['mentor'] = mentor
+                student['major'] = major
+                student['obj'] = Student(id, name, age, mentor, major)
+                return student
+            return student
+
+        if len(Student.all_student) > 0:
+            students_list = map(get_update,Student.all_student)
+
+            with open('students.csv', 'w', newline='') as student:
+                for student_list in students_list:
+                    student_list['obj'] = Student(
+                         student_list['id'],
+                         student_list['name'],
+                         student_list['age'],
+                         student_list['mentor'],
+                         student_list['major']
+                    )
+
+                    student_writer = csv.writer(student)
+                    student_writer.writerow([
+                        student_list['id'],
+                        student_list['name'],
+                        student_list['age'],
+                        student_list['mentor'],
+                        student_list['major'],
+                       ])
+        else:
+            print('')
+            print('No student record found!!!')
+
+    else:
+        print("""
+        Id not found in record
+
+        """)
+        main()
+
+
 
 
 def validate_student_detail(id, name, age, mentor, major):
@@ -64,33 +147,31 @@ def save_student_detail(id, name, age, mentor, major):
 
 def insert_student():
     """
-    Accept Student details
+    Accept Student details..
 
-    Attribute: name is (str), age(int), 
+    Attribute: name is (str), age(int),
     mentor(str), major(str)
     """
-    
-    id = str(input('Enter Student Id: '))
-    name = str(input('Enter Student name: '))
-    age = int(input('Enter Student age: '))
-    mentor = str(input('Enter Student mentor: '))
-    major = str(input('Enter Student major: '))
 
+    id, name, age, mentor, major = accept_initial_input()
     validate_student_detail(id, name, age, mentor, major)
     instantiate_student(id, name, age, mentor, major)
     save_student_detail(id, name, age, mentor, major)
-   
+    print("""
 
-    
-    
+    === Student Details Saved Sucessfully!!! ===
+    """)
 
-  
+
+
+
+
+
 
 def delete_student():
-    """
-    Accept Student Id 
+    """Accept Student Id.
 
-    Use student id to filter student and 
+    Use student id to filter student and
     delete student detail
 
     Attribute: Id is str
@@ -109,21 +190,26 @@ def switch_option(opt):
 
     elif opt == '4':
         delete_student()
-    
-    else:
-        display_options()
-  
 
-            
+    elif opt == '5':
+        return exit()
+
+
+
+
+
 
 
 def main():
-    
+
+    print(' ======Options=====')
     display_options()
 
     option: str = str(input('Select an Option: '))
-    
-    switch_option(option)
+
+    while option != 5:
+        switch_option(option)
+        main()
 
 
 main()
--- my_app/lib/student.py
+++ my_app/lib/student.py
@@ -9,23 +9,24 @@ class Student:
         self.age = age
         self.mentor = mentor
         self.major = major
-        
+
 
     @classmethod
     def instantiate_students(cls, students):
+          cls.all_student = []
           for student in students:
               student_obj = cls(student['id'], student['name'], student['age'], student['mentor'], student['major'] )
               student['obj'] = student_obj
 
               cls.all_student.append(student)
-        
+
           for student in cls.all_student:
-              
+
               print(repr(student['obj']))
 
 
     def __repr__(self):
-            
+
             return f"""
     Id: {self.id}
     Name: {self.name}
@@ -35,5 +36,5 @@ class Student:
 
     ======================
             """
-              
-         
+
+
