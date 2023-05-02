d6537665620d6b39ca3dcef186c657a1a564c00a
ginohmk <michotall95@gmail.com>
2023-05-02 14:51:06 +0100

    feat: Code clean up convert to class

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,4 +1,4 @@
-import csv
+
 from lib.student import Student
 
 def display_options():
@@ -10,236 +10,29 @@ def display_options():
     5. Exist
     """)
 
-def accept_update_input():
-    name = str(input('Enter Student name: '))
-    age = int(input('Enter Student age: '))
-    mentor = str(input('Enter Student mentor: '))
-    major = str(input('Enter Student major: '))
-
-    return  name, age, mentor, major
-
-
-def accept_initial_input():
-    id = str(input('Enter Student Id () : '))
-
-    if valid_id_avialability(id):
-        print("""
-        Id is taken, Use a different Id
-        """)
-        insert_student()
-
-    name = str(input('Enter Student name: '))
-    age = int(input('Enter Student age: '))
-    mentor = str(input('Enter Student mentor: '))
-    major = str(input('Enter Student major: '))
-
-    return id, name, age, mentor, major
-
-
-
-
-
-def select_id():
-    id = str(input('Enter Student Id to update: '))
-    return id
-
-def valid_id_avialability(id):
-
-    for student in Student.all_student:
-        if id == student['id']:
-            return True
-    return False
-
-
-def intsantiate_students_from_file():
-    """Display all students in the csv file.
-    """
-
-    student_obj = {}
-    students_list = []
-    with open("students.csv", "r") as students:
-
-        for student in students:
-            student_list = student.strip().split(',')
-
-            student_obj['id'] = student_list[0].strip()
-            student_obj['name'] =   student_list[1].strip()
-            student_obj['age'] =   student_list[2].strip()
-            student_obj['mentor'] =   student_list[3].strip()
-            student_obj['major'] =   student_list[4].strip()
-
-            students_list.append(student_obj)
-            student_obj = {}
-
-        Student.instantiate_students(students_list)
-
-def update_student():
-
-    """Accept Student Id.
-
-    Use stude id to map student and
-    update student detail
-
-    Attribute: Id is str
-    """
-
-
-    id = select_id()
-
-    if valid_id_avialability(id):
-        name, age, mentor, major = accept_update_input()
-        def get_update(student):
-
-            if student['id'] == id:
-                student['name'] = name
-                student['age'] = age
-                student['mentor'] = mentor
-                student['major'] = major
-                student['obj'] = Student(id, name, age, mentor, major)
-                return student
-            return student
-
-        if len(Student.all_student) > 0:
-            students_list = map(get_update,Student.all_student)
-
-            with open('students.csv', 'w', newline='') as student:
-                for student_list in students_list:
-                    student_list['obj'] = Student(
-                         student_list['id'],
-                         student_list['name'],
-                         student_list['age'],
-                         student_list['mentor'],
-                         student_list['major']
-                    )
-
-                    student_writer = csv.writer(student)
-                    student_writer.writerow([
-                        student_list['id'],
-                        student_list['name'],
-                        student_list['age'],
-                        student_list['mentor'],
-                        student_list['major'],
-                       ])
-        else:
-            print('')
-            print('No student record found!!!')
-
-    else:
-        print("""
-        Id not found in record
-
-        """)
-        main()
-
-
-
-
-def validate_student_detail(id, name, age, mentor, major):
-    # Validation
-    assert isinstance(id, str), "Id must be string"
-    assert isinstance(name, str), "name must be string"
-    assert isinstance(age, int), "age must be integer"
-    assert isinstance(mentor, str), "mentor must be string"
-    assert isinstance(major, str), "major must be string"
-
-def instantiate_student(id, name, age, mentor, major):
-    # Create instance of Student Class
-    Student(id, name, age, mentor, major)
-
-def save_student_detail(id, name, age, mentor, major):
-    with open("students.csv",'a', newline='') as student:
-        student_writer = csv.writer(student)
-        student_writer.writerow([id, name, age, mentor, major])
-
-def insert_student():
-    """
-    Accept Student details..
-
-    Attribute: name is (str), age(int),
-    mentor(str), major(str)
-    """
-
-    id, name, age, mentor, major = accept_initial_input()
-    validate_student_detail(id, name, age, mentor, major)
-
-
-    instantiate_student(id, name, age, mentor, major)
-    save_student_detail(id, name, age, mentor, major)
-    print("""
-
-    === Student Details Saved Sucessfully!!! ===
-    """)
-
-
-
-
-def delete_student():
-    """Accept Student Id.
-
-    Use student id to filter student and
-    delete student detail
-
-    Attribute: Id is str
-    """
-    id = select_id()
-
-
-
-    if valid_id_avialability(id):
-        def filtered_students(student):
-            if (student['id'] != id):
-                return student
-
-        new_students = filter(filtered_students,Student.all_student)
-
-        with open('students.csv', 'w', newline='') as student:
-                for student_list in new_students:
-                    student_list['obj'] = Student(
-                         student_list['id'],
-                         student_list['name'],
-                         student_list['age'],
-                         student_list['mentor'],
-                         student_list['major']
-                    )
-
-                    student_writer = csv.writer(student)
-                    student_writer.writerow([
-                        student_list['id'],
-                        student_list['name'],
-                        student_list['age'],
-                        student_list['mentor'],
-                        student_list['major'],
-                       ])
-
 
 def switch_option(opt):
     if opt == '1':
         Student.display_students()
 
     elif opt == '2':
-        update_student()
+        Student.update_student()
 
     elif opt == '3':
-        insert_student()
+        Student.insert_student()
 
     elif opt == '4':
-        delete_student()
+        Student.delete_student()
 
     elif opt == '5':
         return exit()
 
 
-
-
-
-
-
 def main():
-
-    intsantiate_students_from_file()
-
+    Student.intsantiate_students_from_file()
 
     print(' ======Options=====')
+
     display_options()
 
     option: str = str(input('Select an Option: '))
@@ -248,5 +41,7 @@ def main():
         switch_option(option)
         main()
 
+
+# Main Calling point
 
 main()
--- my_app/lib/student.py
+++ my_app/lib/student.py
@@ -1,31 +1,205 @@
-
+import csv
 
 class Student:
     all_student = []
 
-    def __init__(self, id: str, name: str, age: int, mentor: str, major: str):
-        self.id = id
-        self.name = name
-        self.age = age
-        self.mentor = mentor
-        self.major = major
+    def __init__(self, id, name, age, major, mentor ) -> None:
+         self.id = id
+         self.name = name
+         self.age = age
+         self.major = major
+         self.mentor = mentor
+
+    def accept_initial_input():
+        id = str(input('Enter Student Id () : '))
+
+        if Student.is_valid_id(id):
+            print("""
+            Id is taken, Use a different Id
+            """)
+            Student.accept_initial_input()
+
+        name = str(input('Enter Student name: '))
+        age = int(input('Enter Student age: '))
+        mentor = str(input('Enter Student mentor: '))
+        major = str(input('Enter Student major: '))
+
+        return id, name, age, mentor, major
+
+    def accept_update_input():
+        name = str(input('Enter Student name: '))
+        age = int(input('Enter Student age: '))
+        mentor = str(input('Enter Student mentor: '))
+        major = str(input('Enter Student major: '))
+
+        return  name, age, mentor, major
+
+
+    def insert_student():
+        """
+        Accept Student details..
+
+        Attribute: name is (str), age(int),
+        mentor(str), major(str)
+        """
+
+        id, name, age, mentor, major = Student.accept_initial_input()
+        Student.validate_student_detail(id, name, age, mentor, major)
+        Student.save_student_details_csv_file(id, name, age, mentor, major)
+        print("""
+
+        === Student Details Saved Sucessfully!!! ===
+        """)
+
+    def update_student():
+
+        """Accept Student Id.
+
+        Use stude id to map student and
+        update student detail
+
+        Attribute: Id is str
+        """
+
+        id = Student.select_id()
+
+        if Student.is_valid_id(id):
+            name, age, mentor, major = Student.accept_update_input()
+
+            def get_update(student):
+
+                if student['id'] == id:
+                    student['name'] = name
+                    student['age'] = age
+                    student['mentor'] = mentor
+                    student['major'] = major
+                    student['obj'] = Student(id, name, age, mentor, major)
+                    return student
+                return student
+
+
+            students_list = map(get_update,Student.all_student)
+            Student.write_to_csv_file(students_list)
+
+        else: # No valid Id
+            print("""
+            Id not found in record
+
+            """)
+
+    def delete_student():
+        """Accept Student Id.
+
+        Use student id to filter student and
+        delete student detail
+
+        Attribute: Id is str
+        """
+        id = Student.select_id()
+
+        if Student.is_valid_id(id):
+            def filtered_students(student):
+                if (student['id'] != id):
+                    return student
+
+            new_students = filter(filtered_students,Student.all_student)
+            Student.write_to_csv_file(new_students)
+
+
+
+    def select_id():
+        id = str(input('Enter Student selected Id: '))
+        return id
+
+
+    def save_student_details_csv_file(id, name, age, mentor, major):
+        with open("students.csv",'a', newline='') as student:
+            student_writer = csv.writer(student)
+            student_writer.writerow([id, name, age, mentor, major])
+
+
+    def write_to_csv_file(new_student_list):
+        with open('students.csv', 'w', newline='') as student:
+                    for student_list in new_student_list:
+
+                        student_writer = csv.writer(student)
+                        student_writer.writerow([
+                            student_list['id'],
+                            student_list['name'],
+                            student_list['age'],
+                            student_list['mentor'],
+                            student_list['major'],
+                        ])
+
+
+    def validate_student_detail(id, name, age, mentor, major):
+        # Validation
+        assert isinstance(id, str), "Id must be string"
+        assert isinstance(name, str), "name must be string"
+        assert isinstance(age, int), "age must be integer"
+        assert isinstance(mentor, str), "mentor must be string"
+        assert isinstance(major, str), "major must be string"
+
+    def is_valid_id(id):
+        for student in Student.all_student:
+            if id == student['id']:
+                return True
+        return False
 
     @classmethod
     def display_students(cls):
          print('Students display')
          for student in cls.all_student:
-              print(repr(student['obj']))
 
+              print(Student.__repr__(student['obj']))
 
 
     @classmethod
-    def instantiate_students(cls, students):
-          cls.all_student = []
-          for student in students:
-              student_obj = cls(student['id'], student['name'], student['age'], student['mentor'], student['major'] )
-              student['obj'] = student_obj
+    def intsantiate_students_from_file(cls):
+        """Display all students in the csv file.
 
+
+        Operation: All students are loaded into
+        class property all_student in array of objects
+
+        Attribute: id (str), major (str), mentor (str),
+          name (str), age (int)
+        """
+
+        student_obj = {}
+        students_list = []
+
+        with open("students.csv", "r") as students:
+
+            for student in students:
+                student_list = student.strip().split(',')
+
+                student_obj['id'] = student_list[0].strip()
+                student_obj['name'] =   student_list[1].strip()
+                student_obj['age'] =   student_list[2].strip()
+                student_obj['mentor'] =   student_list[3].strip()
+                student_obj['major'] =   student_list[4].strip()
+
+                students_list.append(student_obj)
+                student_obj = {}
+
+            cls.__instantiate_students(students_list)
+
+
+    @classmethod
+    def __instantiate_students(cls, students):
+
+          cls.all_student = []
+
+          for student in students:
+              student['obj'] = Student(
+                   student['id'],
+                   student['name'],
+                   student['age'],
+                   student['major'],
+                   student['mentor'])
               cls.all_student.append(student)
+
 
 
     def __repr__(self):
